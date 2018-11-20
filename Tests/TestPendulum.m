fd8e15bb7f4a6d816258a3b7433071dd03a61c5c
Vaishnav <vish0908@gmail.com>
2018-11-20 16:27:06 -0600

    help info

--- Tests/TestPendulum.m
+++ Tests/TestPendulum.m
@@ -1,7 +1,11 @@
 % /* This Source Code Form is subject to the terms of the Mozilla Public
 % * License, v. 2.0. If a copy of the MPL was not distributed with this
 % * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
-
+% 
+% This file provides a test case for the user to check their version of the
+% dynamics simulation with the results of a minimum-coordinates
+% realisation, conventionally accepted as the ground truth. 
+% 
 clc; clear;
 
 %% Define initial nodal coordinates and connectivity matrices
--- Tests/testConstraint.m
+++ Tests/testConstraint.m
@@ -2,7 +2,7 @@ function testConstraint(t,y,tData)
 % /* This Source Code Form is subject to the terms of the Mozilla Public
 % * License, v. 2.0. If a copy of the MPL was not distributed with this
 % * file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+% 
 % Function to test constraint violations.
 
 nn = numel(tData.N);
