32c8c602ea05e5a8778a4e0b24c7be4df4ce1e4e
vaishnavtv <vish0908@gmail.com>
2020-02-20 16:47:00 -0600

    -.

--- Main/genKsys.m
+++ Main/genKsys.m
@@ -1,5 +1,5 @@
 function Ksys = genKsys(x,tData)
-% Function to generate Ksys from section 3.3
+% Function to generate Ksys from section 3.2. At equilibrium.
 
 ns = numel(tData.N);
 q = x(1:ns);
@@ -14,11 +14,12 @@ for k = 1:tData.nStr
     Kstr2 = tData.K(k)*tData.Lk(k)/(lsk^3)*Y{k}*(q*q.')*Y{k};
     Ksys = Ksys + Kstr1 + Kstr2;
 end
-
-for j = 1:tData.nBar
-    bj = tData.listX{j}*q;
-    lbj = norm(bj);
-    Kbar1 = tData.psiEq(j)*X{j};
-    Kbar2 = tData.bars.listK(j)*tData.bars.L0(j)/(lbj^3)*X{j}*(q*q.')*X{j};
-    Ksys = Ksys + Kbar1 + Kbar2;
+if(tData.Compressible)
+    for j = 1:tData.nBar
+        bj = tData.listX{j}*q;
+        lbj = norm(bj);
+        Kbar1 = tData.psiEq(j)*X{j};
+        Kbar2 = tData.bars.listK(j)*tData.bars.L0(j)/(lbj^3)*X{j}*(q*q.')*X{j};
+        Ksys = Ksys + Kbar1 + Kbar2;
+    end
 end
